3f8f4a96891e24d84cace6454394c01eeee27ca4
Harsh Raj <2019kuec2045@iiitkota.ac.in>
2023-01-04 11:11:00 +0530

    initial commit

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+// Register the Todo model in mongoose
+require("./todo.model");
+
+//............//
+//const bcrypt=require("bcypt.js");
+//............//
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        todos: {
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: "Todo",
+            default: [],
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
+
